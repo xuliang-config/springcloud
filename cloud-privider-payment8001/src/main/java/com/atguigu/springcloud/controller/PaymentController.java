30c60015409871299874dde1954ec5d083bbcfe4
xuliang <17770048437@163.com>
2021-02-01 09:47:58 +0800

    2021-2-1

--- cloud-privider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-privider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -2,14 +2,10 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
-import com.atguigu.springcloud.entities.T1;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -19,7 +15,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult<Payment> create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("*****插入结果 + " + result);
         if(result > 0){
@@ -30,7 +26,7 @@ public class PaymentController {
     }
 
     @GetMapping(value = "/payment/get/{id}")
-    public CommonResult getPaymentByID(@PathVariable("id") int id){
+    public CommonResult<Payment> getPaymentByID(@PathVariable("id") int id){
         Payment result = paymentService.getPaymentByID(id);
         log.info("*****查询结果 + " + result);
         if(result != null){
