5ef77bf63b7fa85e2d90f35f1125fa05b54db675
xuliang <17770048437@163.com>
2021-03-17 15:55:20 +0800

    2021-3-17

--- cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/impl/MessageSenderImp.java
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/impl/MessageSenderImp.java
@@ -12,7 +12,6 @@ import java.util.UUID;
 @EnableBinding(Source.class)    //定义消息的推送管道
 public class MessageSenderImp implements MessageSender {
 
-
     //消息推送管道，名字最好写成output,这样做最省事，或者指定装配的bean的名字，该类型的Bean有3个
     @Resource
     private MessageChannel output;
@@ -20,6 +19,8 @@ public class MessageSenderImp implements MessageSender {
     @Override
     public String send() {
         String str = UUID.randomUUID().toString();
+        //MessageBuilder.withPayload(str).build()  能生成对应的消息
+        //send方法发送消息
         output.send(MessageBuilder.withPayload(str).build());
         return null;
     }
