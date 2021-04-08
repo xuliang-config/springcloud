f15198d1627c62405710b46abeb1d2f1bf1314c5
xuliang <17770048437@163.com>
2021-04-08 17:32:16 +0800

    2021

--- cloudalibaba-sentinel-service8402/src/main/java/com/atguigu/springcloudalibaba/controller/SentinelController8402.java
+++ cloudalibaba-sentinel-service8402/src/main/java/com/atguigu/springcloudalibaba/controller/SentinelController8402.java
@@ -1,6 +1,9 @@
 package com.atguigu.springcloudalibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +17,18 @@ public class SentinelController8402 {
     @GetMapping("/getB")
     public String getB(){
         return "com from 8402 B";
+    }
+
+    @GetMapping("/getC")
+    @SentinelResource(value = "getC" ,blockHandler = "deal_Exception")
+    public String getC(@RequestParam(value = "id",required = false) String id,
+                       @RequestParam(value = "name",required = false) String name){
+        System.out.println(id);
+        System.out.println(name);
+        return "Hot Key testOK";
+    }
+
+    public String deal_Exception(String id, String name, BlockException ex){
+        return "sentinel热点限流了，返回兜底方法";
     }
 }
